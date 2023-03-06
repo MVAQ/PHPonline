bd63e999a0188b95caae130ef3f85382a8130bc4
Vladimir <vladimirmasalv3@gmail.com>
2023-03-06 17:25:31 +0200

    completed DZ2

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+//scalar
+$int = 1; // integer
+$float = 1.2; // float
+$string = 'ssss';  // string
+$bool = true; // boolean
+$null = null; // null
+
+// complex types
+$array = []; // array
+$function = function () {}; // function callable
+$object = new \stdClass(); // object
+$resource = fopen ("php://stdin","r"); // resource
+var_dump($object);
